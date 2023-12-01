38eec0e17f4cc16ec9e59b65021802f1cbd4a401
Riku Rauhala <riku@rauhala.io>
2023-12-01 22:36:37 +0200

    Update the solution for day 1, part 2

--- 2023/day-01/part_two.py
+++ 2023/day-01/part_two.py
@@ -1,37 +1,26 @@
 def main():
     with open("input.txt", "r") as input_file:
         lines = [line.strip() for line in input_file]
-        sum = 0
 
-        digits = [
-            ["1", "one"],
-            ["2", "two"],
-            ["3", "three"],
-            ["4", "four"],
-            ["5", "five"],
-            ["6", "six"],
-            ["7", "seven"],
-            ["8", "eight"],
-            ["9", "nine"],
-        ]
+        strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+        sum = 0
 
         for line in lines:
             first_appearance = [-1 for _ in range(9)]
             last_appearance = [-1 for _ in range(9)]
 
             for i in range(9):
-                digit = line.find(digits[i][0])
-                letters = line.find(digits[i][1])
+                digit = line.find(str(i+1))
+                letters = line.find(strings[i])
 
                 if digit >= 0 and letters >= 0:
                     first_appearance[i] = min(digit, letters)
-                elif digit >= 0:
-                    first_appearance[i] = digit
-                elif letters >= 0:
-                    first_appearance[i] = letters
+                else:
+                    first_appearance[i] = max(digit, letters)
 
-                digit = line.rfind(digits[i][0])
-                letters = line.rfind(digits[i][1])
+                digit = line.rfind(str(i+1))
+                letters = line.rfind(strings[i])
 
                 if digit >= 0 and letters >= 0:
                     last_appearance[i] = max(digit, letters)
