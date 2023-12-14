550d8ce7798c0f839be259c8afcf98923a80537b
Riku Rauhala <riku@rauhala.io>
2023-12-14 10:08:43 +0200

    Add solution to day 14, part 1

---
+++ 2023/day-14/part_one.py
@@ -0,0 +1,21 @@
+dish = [line.strip() for line in open("input.txt").readlines()]
+tilted = [list(row) for row in zip(*dish)]
+
+for row in tilted:
+    changed = True
+    while changed:
+        changed = False
+        for i in range(1, len(row)):
+            if row[i] == "O" and row[i-1] == ".":
+                row[i], row[i-1] = row[i-1], row[i]
+                changed = True
+
+
+tilted = ["".join(list(row)) for row in zip(*tilted)]
+
+total_load = 0
+
+for i, row in enumerate(tilted):
+    total_load += ((len(tilted)-i) * row.count("O"))
+
+print(total_load)
